b04df404fed68848206690a451e97a8a09289557
Alexandr Zakharov <zakharov.products@gmail.com>
2023-09-03 16:04:19 +0300

    hidden_button_back_for_catalog_header

--- src/copmponents/Header/Header.jsx
+++ src/copmponents/Header/Header.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from 'react';
+
+
 import { Button } from '../UI/Button/Button';
 
 import {ReactComponent as BackIcon} from '../../resources/header/back.svg';
 import {ReactComponent as ExitIcon} from '../../resources/header/exit.svg';
 
+import cn from 'classnames';
 
 import classes from './Header.module.scss';
 
-export const Header = ({children}) => {
+export const Header = ({children, page}) => {
   const [width, setWidth] = useState(window.innerWidth);
 
   useEffect(() => {
@@ -26,8 +29,10 @@ export const Header = ({children}) => {
     <header className={classes.header}>
       <div className="container">
         <nav className={classes.nav}>
-          <Button className={classes.nav__btn}>
-            {width <= 425 ? <BackIcon/> : 'Назад'}
+          <Button
+            disabled={page == 'catalog' ? true : false} 
+            className={page == 'catalog' ? cn(classes.nav__btn, classes.nav__btn_hidden) : classes.nav__btn}>
+            {width <= 425 ? <BackIcon className={classes.svg}/> : 'Назад'}
           </Button>
           <Button className={classes.nav__btn}>
             {width <= 425 ? <ExitIcon/> : 'Выход'}
--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,12 +1,13 @@
 import { EmployeesList } from "../../copmponents/EmployeesList/EmployeesList";
 import { Header } from "../../copmponents/Header/Header"
 
+
 import classes from './Catalog.module.scss';
 
 export const Catalog = () => {
   return (
     <div className="catalog">
-      <Header>
+      <Header page={'catalog'}>
         <h1 className={classes.catalogHeader__title}>Наша команда</h1>
         <div className={classes.catalogHeader__descr}>
           Это опытные специалисты, хорошо разбирающиеся во всех задачах, которые ложатся на их плечи, и умеющие находить выход из любых, даже самых сложных ситуаций. 
