dfc11c4e5e821598521b643d4be8d2fddadf898c
Alexandr Zakharov <zakharov.products@gmail.com>
2023-09-03 13:21:27 +0300

    added_icons_and_media_request_for_header

--- src/copmponents/Header/Header.jsx
+++ src/copmponents/Header/Header.jsx
@@ -1,17 +1,36 @@
+import { useEffect, useState } from 'react';
 import { Button } from '../UI/Button/Button';
+
+import {ReactComponent as BackIcon} from '../../resources/header/back.svg';
+import {ReactComponent as ExitIcon} from '../../resources/header/exit.svg';
+
 
 import classes from './Header.module.scss';
 
 export const Header = ({children}) => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = (e) => {
+      setWidth(e.target.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, [])
+
   return (
     <header className={classes.header}>
       <div className="container">
         <nav className={classes.nav}>
           <Button className={classes.nav__btn}>
-            Назад
+            {width <= 425 ? <BackIcon/> : 'Назад'}
           </Button>
           <Button className={classes.nav__btn}>
-            Выход
+            {width <= 425 ? <ExitIcon/> : 'Выход'}
           </Button>
         </nav>
         {children}
