a36784b6dd611d527b334df5284e0af02a2abac0
Alexandr Zakharov <zakharov.products@gmail.com>
2023-09-04 09:04:47 +0300

    added_btn_more_for_employees

--- src/copmponents/EmployeesList/EmployeesList.jsx
+++ src/copmponents/EmployeesList/EmployeesList.jsx
@@ -1,11 +1,21 @@
+import { Button } from '../UI/Button/Button';
+import {ReactComponent as IconLike} from '../../resources/employees/arrowDown.svg';
+import {ReactComponent as IconArrowDown} from '../../resources/employees/arrowDown.svg';
+
 import classes from './EmployeesList.module.scss';
 
 export const EmployeesList = () => {
   return (
-    <div className="container">
-      <ul className={classes.EmployeesList}>
-      
-      </ul>
-    </div>
+    <section className={classes.employees}>
+      <div className="container">
+        <ul className={classes.employeesList}>
+        
+        </ul>
+        <Button className={classes.employees__more}>
+          Показать еще
+          <IconArrowDown/>
+        </Button>
+      </div>
+    </section>
   )
 }
